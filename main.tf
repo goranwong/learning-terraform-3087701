bcc440417738eed8ab64489ba7d43fe442ddb280
goranwong <157773845+goranwong@users.noreply.github.com>
2026-08-02 12:47:29 -0400

    install tomcat and allow ssh

--- main.tf
+++ main.tf
@@ -23,6 +23,15 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [aws_security_group.blog.id]
 
+# Install Java and Tomcat automatically on Amazon Linux 2023
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y java-17-amazon-corretto-devel tomcat
+              systemctl enable tomcat
+              systemctl start tomcat
+              EOF
+
   tags = {
     Name = "Learning Terraform"
   }
@@ -37,8 +46,8 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 8080
+  to_port     = 8080
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -53,6 +62,15 @@ resource "aws_security_group_rule" "blog_https_in" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id 
+}
+
+resource "aws_security_group_rule" "blog_ssh_in" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] 
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everthing_out" {
